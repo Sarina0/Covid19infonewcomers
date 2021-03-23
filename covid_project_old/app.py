4396c46145ec1ab83f4da951a5fb38604d192708
Hamidreza Haghdust <000438336@student.vcc.ca>
2021-03-22 18:50:25 -0700

    Added older version in covid_project_old folder

---
+++ covid_project_old/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, redirect, url_for, session 
+# from flask_mysqldb import MySQL 
+# import MySQLdb.cursors 
+import re 
+  
+  
+app = Flask(__name__) 
+  
+  
+app.secret_key = 'your secret key'
+  
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'your password'
+app.config['MYSQL_DB'] = 'geeklogin'
+  
+# mysql = MySQL(app) 
+  
+@app.route('/') 
+@app.route('/login', methods =['GET', 'POST']) 
+def login(): 
+    msg = '' 
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form: 
+        username = request.form['username'] 
+        password = request.form['password'] 
+        print(username, password)
+    return render_template('login.html', msg = msg) 
+  
+@app.route('/logout') 
+def logout(): 
+    session.pop('loggedin', None) 
+    session.pop('id', None) 
+    session.pop('username', None) 
+    return redirect(url_for('login')) 
+  
+@app.route('/register', methods =['GET', 'POST']) 
+def register(): 
+    msg = '' 
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form : 
+        username = request.form['username'] 
+        password = request.form['password'] 
+        email = request.form['email'] 
+        print(username, password, email)
+    return render_template('register.html', msg = msg) 
+
+if __name__ == '__main__':
+    app.run(port=8090)
+
+
